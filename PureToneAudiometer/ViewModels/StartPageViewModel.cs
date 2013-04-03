9bccd08e0ac5474acc341d035fc4ec41597097ed
Patryk Cwiek <pcwiek@ubmtechinsights.com>
2013-04-03 10:09:39 +0200

    Added the StartPageView.xaml with a navigation list or NavigationItem.cs.
    Updated Caliburn.Micro from 1.5.0 to 1.5.1

---
+++ PureToneAudiometer/ViewModels/StartPageViewModel.cs
@@ -0,0 +1,50 @@
+﻿namespace PureToneAudiometer.ViewModels
+{
+	using System.Collections.ObjectModel;
+	using System.Windows.Controls;
+	using Caliburn.Micro;
+
+	public class StartPageViewModel : ViewModelBase
+	{
+		public ReadOnlyObservableCollection<NavigationItem> NavigationItems { get; private set; }
+
+		public StartPageViewModel(INavigationService navigationService)
+			: base(navigationService)
+		{
+			NavigationItems =
+				new ReadOnlyObservableCollection<NavigationItem>(
+					new ObservableCollection<NavigationItem>(new[]
+						                                         {
+							                                         new NavigationItem
+								                                         {
+									                                         Glyph = "&#x2649",
+									                                         NavigationName = "Presets",
+									                                         NavigationAction =
+										                                         () =>
+										                                         NavigationService.UriFor<PresetsPageViewModel>()
+										                                                          .Navigate()
+								                                         },
+							                                         new NavigationItem
+								                                         {
+									                                         Glyph = "&#x269C",
+									                                         NavigationName = "Settings",
+									                                         NavigationAction = () => { }
+								                                         }
+						                                         }));
+		}
+
+		public void SelectionChanged(SelectionChangedEventArgs e)
+		{
+			if (e.AddedItems.Count == 0)
+				return;
+
+			var addedItem = e.AddedItems[0] as NavigationItem;
+
+			if (addedItem == null)
+				return;
+
+			addedItem.NavigationAction();
+		}
+
+	}
+}
