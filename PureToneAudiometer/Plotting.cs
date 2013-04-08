45ec0645940025137954a35b2de65ce129359d42
Patryk Ćwiek <patryk.cwiek@gmail.com>
2013-04-08 16:25:15 +0200

    Added OxyPlot core and OxyPlot Silverlight 4 to get plotting to work on Windows Phone 8.
    So far the creation of dedicated WP8 project/DLL failed.

---
+++ PureToneAudiometer/Plotting.cs
@@ -0,0 +1,28 @@
+﻿namespace PureToneAudiometer
+{
+    using OxyPlot;
+    using OxyPlot.Axes;
+
+    public static class Plotting
+    {
+        public static PlotModel CreateLinearAxesModel(string title)
+        {
+            var model = new PlotModel(title);
+
+            model.Axes.Add(new LinearAxis(AxisPosition.Bottom, "Frequency [Hz]"));
+            model.Axes.Add(new LinearAxis(AxisPosition.Left, "Volume [dB HL]"));
+
+            return model;
+        }
+
+        public static PlotModel CreateHalfLogarithmicAxesModel(string title)
+        {
+            var model = new PlotModel(title);
+
+            model.Axes.Add(new LogarithmicAxis(AxisPosition.Bottom, "Frequency [Hz]"));
+            model.Axes.Add(new LinearAxis(AxisPosition.Left, "Volume [dB HL]"));
+
+            return model;
+        }
+    }
+}
